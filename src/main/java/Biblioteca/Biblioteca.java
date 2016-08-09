b1b100487f80add881e01c12a156ba86554b1cbb
sarapiette <spiette@thoughtworks.com>
2016-08-09 17:33:03 -0500

    refactor of biblioteca tests to accept Book Object. Creation of BookTest

--- src/main/java/Biblioteca/Biblioteca.java
+++ src/main/java/Biblioteca/Biblioteca.java
@@ -1,17 +1,33 @@
 package Biblioteca;
 
 import java.io.PrintStream;
+import java.util.ArrayList;
 
 public class Biblioteca {
 
+    private ArrayList<Book> bookList;
     private PrintStream printStream;
 
-    public Biblioteca(PrintStream printStream){
+    public Biblioteca(PrintStream printStream, ArrayList<Book> bookList){
 
         this.printStream = printStream;
+        this.bookList = bookList;
     }
-
+    public void start(){
+        printWelcome();
+        printBookList();
+    }
     public void printWelcome() {
         printStream.println("Welcome to the Biblioteca!");
+    }
+
+    public void printBookList() {
+        if (this.bookList.isEmpty())
+            printStream.println("");
+        else {
+            for (Book book : bookList) {
+                book.print();
+            }
+        }
     }
 }
