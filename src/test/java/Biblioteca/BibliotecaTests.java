aa27c64fe0e1ab1965cb3da61737a8cdde5c550a
sarapiette <spiette@thoughtworks.com>
2016-08-09 17:51:32 -0500

    refactor to move book arraylist into setup method

--- src/test/java/Biblioteca/BibliotecaTests.java
+++ src/test/java/Biblioteca/BibliotecaTests.java
@@ -12,6 +12,7 @@ public class BibliotecaTests {
     private PrintStream printstream;
     private Book book1;
     private Book book2;
+    private ArrayList<Book> books;
 
 
     @Before
@@ -19,11 +20,11 @@ public class BibliotecaTests {
         printstream = mock(PrintStream.class);
         book1 = mock(Book.class);
         book2 = mock(Book.class);
+        books = new ArrayList<Book>();
     }
 
     @Test
     public void shouldPrintWelcomeWhenApplicationStarts() {
-
         Biblioteca biblioteca = new Biblioteca(printstream, new ArrayList<Book>());
         biblioteca.printWelcome();
         verify(printstream).println("Welcome to the Biblioteca!");
@@ -32,8 +33,7 @@ public class BibliotecaTests {
 
     @Test
     public void shouldPrintEmptyStringWhenLibraryEmpty() {
-
-        Biblioteca biblioteca = new Biblioteca(printstream, new ArrayList<Book>());
+        Biblioteca biblioteca = new Biblioteca(printstream, books);
         biblioteca.printBookList();
         verify(printstream).println("");
 
@@ -41,7 +41,6 @@ public class BibliotecaTests {
 
     @Test
     public void shouldPrintOneBookWhenLibraryHasOneBook() {
-        ArrayList<Book> books = new ArrayList<Book>();
         books.add(book1);
         Biblioteca biblioteca = new Biblioteca(printstream, books);
         biblioteca.printBookList();
@@ -50,10 +49,8 @@ public class BibliotecaTests {
 
     @Test
     public void shouldPrintTwoBooksWhenLibraryHasTwoBooks() {
-        ArrayList<Book> books = new ArrayList<Book>();
         books.add(book1);
         books.add(book2);
-
         Biblioteca biblioteca = new Biblioteca(printstream, books);
         biblioteca.printBookList();
         verify(book1).print();
